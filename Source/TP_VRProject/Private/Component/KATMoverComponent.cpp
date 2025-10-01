cd7ca9f75fb53cb186ee7abeecee94be55a13fa8
narisawa hibiki <yopukyaga@gmail.com>
2025-10-01 15:15:57 +0900

    プラグインの追加

---
+++ Source/TP_VRProject/Private/Component/KATMoverComponent.cpp
@@ -0,0 +1,99 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/KATMoverComponent.h"
+#include "KATSDKWarpper.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values for this component's properties
+UKATMoverComponent::UKATMoverComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UKATMoverComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+	KATDataHandler = new KATSDKWarpper();
+	VRCamera = Cast<UCameraComponent>(GetOwner()->FindComponentByClass(UCameraComponent::StaticClass()));
+}
+
+
+// Called every frame
+void UKATMoverComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	HandleKATVRInput(DeltaTime);
+}
+
+void UKATMoverComponent::StartMove()
+{
+	CanMove = true;
+}
+
+void UKATMoverComponent::StopMove()
+{
+	CanMove = false;
+}
+
+void UKATMoverComponent::HandleKATVRInput(float DeltaTime)
+{
+	HandleKATVRInputWalk(DeltaTime);
+	HandleKATVRRotator();
+}
+
+void UKATMoverComponent::HandleKATVRInputWalk(float DeltaTime)
+{
+	if (!CanMove) return;
+
+	if (VRCamera == nullptr) return;
+
+	Vector3 MoveSpeed = KATDataHandler->GetWalkStatus(nullptr).treadMillData.moveSpeed;
+
+	//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Move: %s"), *Vector3::toFVector(MoveSpeed).ToString()));
+
+	float MoveAmount = MoveSpeed.z;
+	auto cameraForwardVector = FVector(VRCamera->GetForwardVector().X, VRCamera->GetForwardVector().Y, 0).GetSafeNormal();
+	//FootstepCounter += FMath::Abs(DeltaTime * NowSpeed * MoveAmount);
+
+	GetOwner()->AddActorWorldOffset(cameraForwardVector * MoveAmount * NowSpeed,true);
+	//AddActorWorldOffset(GetActorForwardVector() * MoveAmount * NowSpeed);
+}
+
+void UKATMoverComponent::HandleKATVRRotator()
+{
+	FQuat newQuat = Quaternion::unityToUnreal(KATDataHandler->GetWalkStatus(nullptr).treadMillData.bodyRotationRaw);
+
+	RotateCharacterByFQuat(newQuat, 0.5f);
+}
+
+void UKATMoverComponent::RotateCharacterByFQuat(FQuat targetQuat, float duration)
+{
+	//if (!bCanRotate) return;
+
+	CurrentRotator = targetQuat.Rotator();
+	FRotator DeltaRotation = CurrentRotator - PreRotator;
+
+	DeltaRotation.Yaw = -DeltaRotation.Yaw;
+
+	FRotator CharacterTargetRotator = GetOwner()->GetActorRotation() + DeltaRotation;
+
+	FRotator VROffsetRotator = GetRelativeRotation() - DeltaRotation;
+
+	GetOwner()->SetActorRotation(CharacterTargetRotator);
+
+	SetRelativeRotation(VROffsetRotator);
+
+	PreRotator = CurrentRotator;
+}
+
---
+++ Source/TP_VRProject/Public/Component/KATMoverComponent.h
@@ -0,0 +1,101 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "KATMoverComponent.generated.h"
+
+class KATSDKWarpper;
+class UCameraComponent;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TP_VRPROJECT_API UKATMoverComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UKATMoverComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	/// <summary>
+	/// 移動開始
+	/// </summary>
+	void StartMove();
+
+	/// <summary>
+	/// 移動停止
+	/// </summary>
+	void StopMove();
+
+	/// <summary>
+	/// KATVRのキャリブレーションを実行
+	/// </summary>
+
+
+private:
+
+	/// <summary>
+	/// KATVRからの入力処理
+	/// </summary>
+	/// <param name="DeltaTime"></param>
+	void HandleKATVRInput(float DeltaTime);
+
+	/// <summary>
+	/// 歩く処理
+	/// </summary>
+	/// <param name="DeltaTime"></param>
+	void HandleKATVRInputWalk(float DeltaTime);
+
+	/// <summary>
+	/// 回転処理
+	/// </summary>
+	void HandleKATVRRotator();
+
+	/// <summary>
+	/// キャラクターに反応する
+	/// </summary>
+	/// <param name="targetQuat"></param>
+	/// <param name="duration"></param>
+	void RotateCharacterByFQuat(FQuat targetQuat, float duration);
+
+	/// <summary>
+	/// KATVR
+	/// </summary>
+	KATSDKWarpper* KATDataHandler;
+	
+	/// <summary>
+	/// カメラ
+	/// </summary>
+	UCameraComponent* VRCamera;
+
+	/// <summary>
+	/// 前回の回転角度
+	/// </summary>
+	FRotator PreRotator;
+
+	/// <summary>
+	/// 今回の回転角度
+	/// </summary>
+	FRotator CurrentRotator;
+
+	/// <summary>
+	/// 移動速度
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float NowSpeed = 4.0f;
+
+	/// <summary>
+	/// 移動できるか
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool CanMove = true;
+};
