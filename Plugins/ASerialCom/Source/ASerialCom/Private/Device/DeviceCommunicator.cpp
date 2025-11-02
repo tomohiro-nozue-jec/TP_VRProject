b6e7f68af6094539becfc5dba0ad1c99793a2e27
gene <cg100111@gmail.com>
2025-11-03 01:20:38 +0900

    基本のスレッド

---
+++ Plugins/ASerialCom/Source/ASerialCom/Private/Device/DeviceCommunicator.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Device/DeviceCommunicator.h"
+#include "ASerialLibControllerWin.h"
+
+DeviceCommunicator::DeviceCommunicator(const TCHAR* Name)
+	: UpdateFrequency(60.0f)
+	, bIsRunning(true)
+	, bIsPause(false)
+{
+	Thread = FRunnableThread::Create(this, Name, 0, TPri_Normal);
+}
+
+DeviceCommunicator::~DeviceCommunicator()
+{
+	if (Thread)
+	{
+		//Thread->Kill(true);
+		EnsureCompletion();
+		delete Thread;
+		Thread = nullptr;
+	}
+}
+
+bool DeviceCommunicator::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Device thread initialized."));
+	// 
+	return false;
+}
+
+uint32 DeviceCommunicator::Run()
+{
+	while (bIsRunning)
+	{
+		if (bIsPause)
+			continue;
+
+
+	}
+	return 0;
+}
+
+void DeviceCommunicator::Stop()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Device thread stopped."));
+	bIsRunning = false;
+}
+
+void DeviceCommunicator::Exit()
+{
+
+}
+
+void DeviceCommunicator::EnsureCompletion()
+{
+	Stop();
+
+	// スレッドが存在する場合、完了を待つ
+	if (Thread)
+	{
+		Thread->WaitForCompletion();
+	}
+}
+
+void DeviceCommunicator::Pause()
+{
+	bIsPause = true;
+}
+
+void DeviceCommunicator::ReStart()
+{
+	bIsPause = false;
+}
---
+++ Plugins/ASerialCom/Source/ASerialCom/Public/Device/DeviceCommunicator.h
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HAL/Runnable.h"
+#include "HAL/RunnableThread.h"
+
+class UASerialLibControllerWin;
+
+/**
+ * 
+ */
+class ASERIALCOM_API DeviceCommunicator : public FRunnable
+{
+public:
+	DeviceCommunicator(const TCHAR* Name);
+	~DeviceCommunicator();
+
+	virtual bool Init() override;
+	virtual uint32 Run() override;
+	virtual void Stop() override;
+	virtual void Exit() override;
+
+#pragma region スレッド関連
+public:
+
+	/// <summary>
+	/// 安全に終了する
+	/// </summary>
+	void EnsureCompletion();
+
+	/// <summary>
+	/// 一時停止する
+	/// </summary>
+	void Pause();
+
+	/// <summary>
+	/// 再開する
+	/// </summary>
+	void ReStart();
+
+private:
+
+	/// <summary>
+	/// 今のスレッドを管理する
+	/// </summary>
+	FRunnableThread* Thread;
+
+	/// <summary>
+	/// 更新頻度(デフォルト：60)
+	/// </summary>
+	float UpdateFrequency;
+
+	/// <summary>
+	/// 動けるか
+	/// </summary>
+	bool bIsRunning;
+
+	/// <summary>
+	/// 一時停止フラグ
+	/// </summary>
+	bool bIsPause;
+
+#pragma endregion
+
+};
