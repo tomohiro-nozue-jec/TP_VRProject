af623ae5cf3ff583a3033b47ed28164ab5b2825a
tomohiro-nozue-jec <tomohiro.nozue@jec.ac.jp>
2025-11-09 18:32:50 +0900

    UTF-8 BOM追加とヘッダー/モジュール初期化の修正
    StatefulActivatableInterface.cpp:
    - コメントの文字コードをUTF-8 BOM付きに変更。
    - `#include "Interface/StatefulActivatableInterface.h"` を追加。
    
    TP_VRProject.cpp:
    - コメントの文字コードをUTF-8 BOM付きに変更。
    - 必要なヘッダーをインクルード。
    - `IMPLEMENT_PRIMARY_GAME_MODULE` を使用してモジュールを初期化。
    
    TP_VRProject.h:
    - コメントの文字コードをUTF-8 BOM付きに変更。
    - `#pragma once` と `#include "CoreMinimal.h"` を追加。

--- Source/TP_VRProject/TP_VRProject.cpp
+++ Source/TP_VRProject/TP_VRProject.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TP_VRProject.h"
 #include "Modules/ModuleManager.h"
