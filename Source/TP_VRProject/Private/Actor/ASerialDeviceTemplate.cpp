8ae515ffb72ba35e37c402097b8b3acfe459fcf3
gene <cg100111@gmail.com>
2025-11-14 12:17:29 +0900

    見本用のマップ作った。 C++とBPそれぞれの見本を用意した

---
+++ Source/TP_VRProject/Private/Actor/ASerialDeviceTemplate.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/ASerialDeviceTemplate.h"
+#include "ASerialComBPLibrary.h"
+#include "Device/WiredDevice.h"
+
+// Sets default values
+AASerialDeviceTemplate::AASerialDeviceTemplate()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AASerialDeviceTemplate::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// デバイスの初期化
+	InitializeDevice();
+
+	// シリアルデバイスの作成, ID, バージョン, 名前を指定する
+	SerialDevice = UASerialComBPLibrary::CreateDevice(WiredDeviceID, WiredDeviceVer, "Test");
+	if (SerialDevice)
+	{
+		// デバイスからデータを受け取ったときのイベントを登録
+		SerialDevice->OnRecieveDataEvent.AddDynamic(this, &AASerialDeviceTemplate::OnDataReceived);
+	}
+}
+
+// Called every frame
+void AASerialDeviceTemplate::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AASerialDeviceTemplate::SendCmd()
+{
+	if (SerialDevice && SerialDevice->IsConnecting())
+	{
+		SerialDevice->RequestData(RPMCmd);
+	}
+}
+
+void AASerialDeviceTemplate::InitializeDevice_Implementation()
+{
+	// これはエルゴメーターのデータ、実際どうなるかは電子科から貰った資料を見てください
+	// 有線デバイスのIDとバージョンを設定
+	WiredDeviceID = 0x03;
+	WiredDeviceVer = 0x03;
+
+	// 取得したいデータのコマンドを設定
+	RPMCmd = 0x21;
+}
+
+void AASerialDeviceTemplate::OnDataReceived(int Value)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Received Data: %d"), Value));
+	// ここで受け取ったデータを処理
+
+	// ずっとデータを要求し続ける場合、前のデータを受け取った後に再度コマンドを送信
+	UE_LOG(LogTemp, Display, TEXT("Send Command Again"));
+	SendCmd();
+}
+
---
+++ Source/TP_VRProject/Public/Actor/ASerialDeviceTemplate.h
@@ -0,0 +1,79 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ASerialDeviceTemplate.generated.h"
+
+class UWiredDevice;
+
+UCLASS()
+class TP_VRPROJECT_API AASerialDeviceTemplate : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AASerialDeviceTemplate();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	/// <summary>
+	/// コマンドを送信
+	/// </summary>
+	UFUNCTION(BlueprintCallable)
+	void SendCmd();
+
+	void Connect();
+
+	void Disconnect();
+
+	void PauseThread();
+
+	void ReStartThread();
+
+protected:
+
+	/// <summary>
+	/// デバイスを初期化
+	/// </summary>
+	UFUNCTION(BlueprintNativeEvent)
+	void InitializeDevice();
+
+private:
+
+	/// <summary>
+	/// デバイスからデータを受け取ったときの処理
+	/// </summary>
+	UFUNCTION()
+	void OnDataReceived(int Value);
+
+	/// <summary>
+	/// デバイス
+	/// </summary>
+	/// ここに必ずUPROPERTY()をつけること。UWiredDeviceはUObjectであるため、付けないとガベージコレクションで消される可能性がある。
+	UPROPERTY()
+	UWiredDevice* SerialDevice;
+
+	/// <summary>
+	/// 有線デバイスのID
+	/// </summary>
+	int WiredDeviceID;
+
+	/// <summary>
+	/// 有線デバイスのバージョン
+	/// </summary>
+	int WiredDeviceVer;
+
+	/// <summary>
+	/// RPMのコマンド
+	/// </summary>
+	unsigned char RPMCmd;
+};
